b7142ff07bb780790fcf892bf69d626d37e0f2ba
Alexander Ewetumo <trinoxf@gmail.com>
2018-10-27 12:06:31 +0100

    Add event validation in tests for actor

--- actor_impl.go
+++ actor_impl.go
@@ -396,7 +396,7 @@ func (ati *ActorImpl) Start(data interface{}) ErrWaiter {
 
 	// signal children of actor to start as well.
 	ati.tree.Each(func(actor Actor) bool {
-		chain.Go(func() error {
+		chain = chain.Then(func() error {
 			return actor.Start(data).Wait()
 		})
 		return true
@@ -460,7 +460,7 @@ func (ati *ActorImpl) Stop(data interface{}) ErrWaiter {
 	})
 
 	ati.tree.Each(func(actor Actor) bool {
-		chain.Go(func() error {
+		chain = chain.Then(func() error {
 			return actor.Stop(data).Wait()
 		})
 		return true
@@ -492,6 +492,8 @@ func (ati *ActorImpl) Restart(data interface{}) ErrWaiter {
 	}).Then(func() error {
 		ati.restarting.Off()
 
+		return ati.Start(data).Wait()
+	}).Then(func() error {
 		ati.events.Publish(ActorRestarted{
 			Data: data,
 			Addr: ati.Addr(),
@@ -499,8 +501,6 @@ func (ati *ActorImpl) Restart(data interface{}) ErrWaiter {
 		})
 
 		return nil
-	}).Then(func() error {
-		return ati.Start(data).Wait()
 	})
 
 	return chain
--- actor_impl_test.go
+++ actor_impl_test.go
@@ -11,7 +11,6 @@ import (
 type basic struct{}
 
 func (b basic) Action(addr actorkit.Addr, env actorkit.Envelope) {
-
 }
 
 func TestActorImpl(t *testing.T) {
@@ -21,12 +20,57 @@ func TestActorImpl(t *testing.T) {
 		actorkit.UseBehaviour(basic{}),
 	)
 
-	assert.NoError(t, am.Start("ready").Wait())
+	specs := []func(interface{}){
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStartRequested{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStarted{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorRestartRequested{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStopRequested{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStopped{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStartRequested{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStarted{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorRestarted{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStopRequested{}, i)
+		},
+		func(i interface{}) {
+			assert.IsType(t, actorkit.ActorStopped{}, i)
+		},
+	}
+
+	var events []interface{}
+	sub := am.Watch(func(i interface{}) {
+		events = append(events, i)
+	})
+
+	defer sub.Stop()
+
+	assert.NoError(t, am.Start("start").Wait())
 	assert.False(t, am.Stopped())
 
-	assert.NoError(t, am.Restart("ready").Wait())
+	assert.NoError(t, am.Restart("restart").Wait())
 	assert.False(t, am.Stopped())
 
-	assert.NoError(t, am.Stop("end").Wait())
+	assert.NoError(t, am.Stop("stop").Wait())
 	assert.True(t, am.Stopped())
+
+	for ind, elem := range events {
+		spec := specs[ind]
+		spec(elem)
+	}
 }
