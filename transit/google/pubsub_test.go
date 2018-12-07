21b8d74e33b06e597418f3a1522ba75583db2129
Alexander Ewetumo <trinoxf@gmail.com>
2018-12-07 21:57:03 +0800

    Add tests for redis and google pubsub transits pkgs

--- transit/google/pubsub_test.go
+++ transit/google/pubsub_test.go
@@ -1,1 +1,48 @@
 package google_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	gpubsub "cloud.google.com/go/pubsub"
+	"github.com/gokit/actorkit"
+	"github.com/gokit/actorkit/transit"
+	"github.com/gokit/actorkit/transit/google"
+	"github.com/gokit/actorkit/transit/internal/benches"
+	"github.com/gokit/actorkit/transit/internal/encoders"
+)
+
+func TestKafka(t *testing.T) {
+	publishers, err := google.NewPublisherFactory(context.Background(), google.PublisherConfig{
+		ProjectID:          "",
+		CreateMissingTopic: true,
+		Marshaler: &google.PubSubMarshaler{
+			Marshaler: encoders.NoAddressMarshaler{},
+		},
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, publishers)
+
+	subscribers, err := google.NewSubscriptionFactory(context.Background(), google.SubscriberConfig{
+		ProjectID: "",
+		Unmarshaler: &google.PubSubUnmarshaler{
+			Unmarshaler: encoders.NoAddressUnmarshaler{},
+		},
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, subscribers)
+
+	factory := google.PubSubFactory(func(factory *google.PublisherFactory, topic string) (transit.Publisher, error) {
+		return factory.Publisher(topic, &gpubsub.PublishSettings{})
+	}, func(factory *google.SubscriptionFactory, topic string, id string, receiver transit.Receiver) (actorkit.Subscription, error) {
+		return factory.Subscribe(topic, id, &gpubsub.SubscriptionConfig{}, receiver, func(_ error) google.Directive {
+			return google.Nack
+		})
+	})(publishers, subscribers)
+
+	benches.PubSubFactoryTestSuite(t, factory)
+}
--- transit/natstreaming/streaming_test.go
+++ transit/natstreaming/streaming_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gokit/actorkit/transit/internal/encoders"
 	"github.com/stretchr/testify/assert"
 
-	nats "github.com/gokit/actorkit/transit/natstreaming"
+	streaming "github.com/gokit/actorkit/transit/natstreaming"
 
 	"github.com/gokit/actorkit"
 	"github.com/gokit/actorkit/transit"
@@ -15,7 +15,7 @@ import (
 )
 
 func TestNATS(t *testing.T) {
-	natspub, err := nats.NewPublisherSubscriberFactory(context.Background(), nats.Config{
+	natspub, err := streaming.NewPublisherSubscriberFactory(context.Background(), streaming.Config{
 		URL:         "",
 		ClusterID:   "",
 		Marshaler:   encoders.NoAddressMarshaler{},
@@ -25,11 +25,11 @@ func TestNATS(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, natspub)
 
-	factory := nats.PubSubFactory(func(factory *nats.PublisherSubscriberFactory, topic string) (transit.Publisher, error) {
+	factory := streaming.PubSubFactory(func(factory *streaming.PublisherSubscriberFactory, topic string) (transit.Publisher, error) {
 		return factory.Publisher(topic)
-	}, func(factory *nats.PublisherSubscriberFactory, topic string, id string, receiver transit.Receiver) (actorkit.Subscription, error) {
-		return factory.Subscribe(topic, id, receiver, func(_ error) nats.Directive {
-			return nats.Nack
+	}, func(factory *streaming.PublisherSubscriberFactory, topic string, id string, receiver transit.Receiver) (actorkit.Subscription, error) {
+		return factory.Subscribe(topic, id, receiver, func(_ error) streaming.Directive {
+			return streaming.Nack
 		}, nil)
 	})(natspub)
 
