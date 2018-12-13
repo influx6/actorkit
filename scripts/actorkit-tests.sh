f5257a68c0543e348885147b3af3acd17117434c
Alexander Ewetumo <trinoxf@gmail.com>
2018-12-13 08:09:02 +0800

    Update scripts

--- scripts/actorkit-tests.sh
+++ scripts/actorkit-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+env CGO_ENABLED=0
+
 echo "Prerun: Verify go modules..."
 go mod verify
 echo "Prerun: Tidying any missing or currupted modules..."
@@ -9,12 +11,12 @@ sleep 5
 
 echo "Run cover tests for retries"
 go test -v -cover ./retries
-go test -v -race ./retries
+go test -v ./retries
 
 echo "Run cover tests for platform"
 go test -v -cover ./platform
-go test -v -race ./platform
+go test -v ./platform
 
 echo "Run cover tests for actorkit"
 go test -v -cover .
-go test -v -race .
+go test -v .
--- scripts/redis-tests.sh
+++ scripts/redis-tests.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+env CGO_ENABLED=0
+
+echo "Prerun: Verify go modules..."
+go mod verify
+echo "Prerun: Tidying any missing or currupted modules..."
+go mod tidy
+
+sleep 5
+
+echo "Run cover tests for retries"
+go test -v -cover ./retries
+go test -v ./retries
+
+echo "Run cover tests for platform"
+go test -v -cover ./platform
+go test -v ./platform
+
+echo "Run cover tests for actorkit"
+go test -v -cover .
+go test -v .
