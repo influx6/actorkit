3d7826fe0810d81702d2f711c5eccc6c46e58630
Alexander Ewetumo <trinoxf@gmail.com>
2019-01-28 21:40:27 +0800

    UPT: Google pubsub tests is wacky, passes at times, does not get message

--- tools/google-pubsub-local-run.sh
+++ tools/google-pubsub-local-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-env PUBSUB_EMULATOR_HOST=localhost:8738 PUBSUB_PROJECT_ID=actorkit \
-gcloud beta emulators pubsub start --host-port=127.0.0.1:8738 --data-dir=./data --log-http --verbosity=debug --user-output-enabled
+env PUBSUB_EMULATOR_HOST=localhost:8738 PUBSUB_PROJECT_ID="actorkit" \
+gcloud beta emulators pubsub start --host-port=localhost:8738 --project="actorkit" --data-dir=./data --log-http --verbosity=debug --user-output-enabled
