7f53d98690e42794f0ecc8f778ddaac6c8b54de5
Alexander Ewetumo <trinoxf@gmail.com>
2019-01-30 17:01:21 +0800

    ADD: Add initial runtimes pkg

--- runtimes/runtimes.go
+++ runtimes/runtimes.go
@@ -1,1 +1,3 @@
+// Package runtimes implements different management runtime which connects to different communication channels to initiate
+// and distribute work for a deployed actorkit system.
 package runtimes
