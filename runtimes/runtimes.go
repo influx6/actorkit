65975fd40775e91fbade6bf9360656036a76d6a4
Alexander Ewetumo <trinoxf@gmail.com>
2019-02-15 08:00:29 +0800

    UPT: Remove reservation from runtimes

--- runtimes/runtimes.go
+++ runtimes/runtimes.go
@@ -1,8 +1,3 @@
 // Package runtimes implements different management runtime which connects to different communication channels to initiate
 // and distribute work for a deployed actorkit system.
 package runtimes
-
-type Proc struct {
-	Event string `json:"event"`
-	Reservation
-}
